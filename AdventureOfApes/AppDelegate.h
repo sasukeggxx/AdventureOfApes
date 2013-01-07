2908c2d862f6bd245b3ec10dd59cd10ce71c2b0b
sasukeggxx <jobzen@gmail.com>
2013-01-07 14:34:36 +0800

    Initial Commit

---
+++ AdventureOfApes/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  AdventureOfApes
+//
+//  Created by Alex Zen on 13-1-7.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ AdventureOfApes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  AdventureOfApes
+//
+//  Created by Alex Zen on 13-1-7.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
